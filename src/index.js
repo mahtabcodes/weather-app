40133245dcd5ad1371bc9f8d353373dad582cb03
mahtabcodes <109015469+mahtabcodes@users.noreply.github.com>
2022-08-09 17:49:06 +0430

    synced emojis

--- src/index.js
+++ src/index.js
@@ -36,7 +36,6 @@ let cityC = city.innerHTML;
 let apiKey = "c7b5ae0ee7938381fd83d5fdfc195c15";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric`;
 function showTemperature(response) {
-  console.log(response.data);
   let temperature = Math.round(response.data.main.temp);
   let tempC = document.querySelector("#temp-center");
   tempC.innerHTML = temperature;
@@ -61,11 +60,15 @@ function showTemperature(response) {
   city.innerHTML = response.data.name;
   let dateElement = document.querySelector("#date-real-time");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#weather-icon-today");
+  iconElement.setAttribute(
+    "src",
+    `./emoji/${response.data.weather[0].icon}.png`
+  );
 }
 axios.get(`${apiUrl}&q=${cityC}&appid=${apiKey}`).then(showTemperature);
 
 //searchInput to cityplace
-
 function search(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
